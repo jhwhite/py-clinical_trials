5c3f0a91338e92a598ca096aaa179246f34d1646
Jody White <jody@jody-white.com>
2015-06-19 20:21:05 -0400

    Added ability to toggle highlight and italic for search terms on trial page

--- static/custom_js.js
+++ static/custom_js.js
@@ -1,3 +1,20 @@
+// function removeEmTag(){
+//     var em = document.getElementsByTagName('em');
+
+//     while(em.length) {
+//         var parent = em[ 0 ].parentNode;
+//         while( em[ 0 ].firstChild ) {
+//             parent.insertBefore(  em[ 0 ].firstChild, em[ 0 ] );
+//         }
+//          parent.removeChild( em[ 0 ] );
+//     }
+// }
+
+function toggleEmClass() {
+    $( "em" ).toggleClass( "highlight" );
+
+}
+
 $(document).ready(function(){
     $('#search-help').hide();
     $(".show-help").click(function(){
@@ -6,4 +23,6 @@ $(document).ready(function(){
     $(".btn2").click(function(){
         $("p").slideDown();
     });
+
+
 });
