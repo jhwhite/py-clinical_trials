5a8928a29cf311cf0f039299e209c580e15b790e
Jody White <jody@jody-white.com>
2015-06-19 21:23:47 -0400

    Removed superfluous code

--- static/custom_js.js
+++ static/custom_js.js
@@ -1,15 +1,3 @@
-// function removeEmTag(){
-//     var em = document.getElementsByTagName('em');
-
-//     while(em.length) {
-//         var parent = em[ 0 ].parentNode;
-//         while( em[ 0 ].firstChild ) {
-//             parent.insertBefore(  em[ 0 ].firstChild, em[ 0 ] );
-//         }
-//          parent.removeChild( em[ 0 ] );
-//     }
-// }
-
 function toggleEmClass() {
     $( "em" ).toggleClass( "highlight" );
 
